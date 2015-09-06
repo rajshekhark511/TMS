203eb1633d78e2736af7aff89ced71f01bddf21d
vipuly <yerawarvips@gmail.com>
2015-09-06 19:18:48 +0530

    Revert "Added project"
    This reverts commit 49ba53971490e8646067c4ea07fa00c13d569d60.

--- application/views/common/js.php
+++
@@ -1,7 +0,0 @@
-<script src="<?php echo base_url('js/jquery-1.11.1.js');?>" type="text/javascript"></script>
-<script src="<?php echo base_url('js/modernizr-2.6.2-respond-1.1.0.min.js');?>"  type="text/javascript"></script>
-<script src="<?php echo base_url('js/jquery.slitslider.js');?>"  type="text/javascript"></script>
-<script src="<?php echo base_url('js/jquery.ba-cond.min.js');?>"  type="text/javascript"></script>
-<script src="<?php echo base_url('js/jquery.validate.js');?>"  type="text/javascript"></script>
-<script src="<?php echo base_url('js/additional-methods.js');?>"  type="text/javascript"></script>
-<script src="<?php echo base_url('js/custom.js');?>"  type="text/javascript"></script>
